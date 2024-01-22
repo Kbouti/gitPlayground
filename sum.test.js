c32d219f3dbc0f8e3af86990d821451a02db90a7
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-01-22 11:32:53 -0800

    Add various test examples

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,63 @@
 const sum = require(`./sum`);
 
 test(`adds 1 + 2 to equal 3`, () => {
-    expect(sum(1,2)).toBe(3);
-})
+  expect(sum(1, 2)).toBe(3);
+});
+
+test(`adds 5 + 64 to equal 69`, () => {
+  expect(sum(5, 64)).toBe(69);
+});
+
+test("adding positive numbers is not zero", () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(sum(a, b)).not.toBe(0);
+    }
+  }
+});
+
+test("adding floating point numbers", () => {
+  const value = 0.1 + 0.2;
+  //expect(value).toBe(0.3);           This won't work because of rounding error
+  expect(value).toBeCloseTo(0.3); // This works.
+});
+
+test("null", () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+test("zero", () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
+
+test("there is no I in team", () => {
+  expect("team").not.toMatch(/I/);
+});
+
+test('but there is a "stop" in Christoph', () => {
+  expect("Christoph").toMatch(/stop/);
+});
+
+
+const shoppingList = [
+    'diapers',
+    'kleenex',
+    'trash bags',
+    'paper towels',
+    'milk',
+  ];
+  
+  test('the shopping list has milk on it', () => {
+    expect(shoppingList).toContain('milk');
+    expect(new Set(shoppingList)).toContain('milk');
+  });
